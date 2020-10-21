f10a19cda095c02ec69dd34e4a3f428de5ddf2c7
sushma-thandle <sushmathandle@gmail.com>
2020-10-21 22:42:44 +0530

    Buffio demo

---
+++ systcall/buffio.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+int main(int argc,char const *argv[]){
+    printf("hello\n");
+    printf("world\n");
+    return 0;
+}
