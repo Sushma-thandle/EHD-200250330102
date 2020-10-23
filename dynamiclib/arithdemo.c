b19d710525483dd0b7593f0d662e37fcabc2f00c
sushma-thandle <sushmathandle@gmail.com>
2020-10-23 20:59:30 +0530

    staticlib and dynamiclib

---
+++ dynamiclib/arithdemo.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+
+int main(int argc,char const *argv[]){
+    printf("add:%d\n",add(10,20));
+    printf("sub:%d\n",sub(20,10));
+    return 0;
+}
---
+++ dynamiclib/pune/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+
+int main(int argc,char const *argv[]){
+    printf("friends code of add:%d\n",add(10,20));
+  
+    return 0;
+}
