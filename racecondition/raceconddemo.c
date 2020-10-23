fb69a7bca9d9dff81a7e1b0d1cf04aa657b00168
sushma-thandle <sushmathandle@gmail.com>
2020-10-23 13:51:05 +0530

    racecondition demo

---
+++ racecondition/raceconddemo.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+
+int count=0;
+
+void *inc_thread(void *argv){
+    while(1){
+    count++;
+    printf("inc_thread:%d\n",count);
+    }
+}
+
+void *dec_thread(void *argv){
+    while(1){
+    count--;
+    printf("dec_thread:%d\n",count);
+    }
+}
+
+int main(int argc,char const *argv[]){
+    pthread_t tid_inc,tid_dec;
+    pthread_create(&tid_inc,NULL,inc_thread,NULL);
+    pthread_create(&tid_dec,NULL,dec_thread,NULL);
+ 
+    pthread_join(tid_inc,NULL);
+    pthread_join(tid_dec,NULL);
+    return 0;
+}
